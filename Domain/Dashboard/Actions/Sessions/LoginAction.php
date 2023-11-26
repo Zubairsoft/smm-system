6e1101a2ae1e2cb5c2063cad74809007724fb077
Mohammed Zubair <moh.775123554@gmail.com>
2023-11-26 22:14:24 +0300

    Add login action

---
+++ Domain/Dashboard/Actions/Sessions/LoginAction.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Domain\Dashboard\Actions\Sessions;
+
+use App\Exceptions\LogicException;
+use App\Models\Admin;
+use Domain\Dashboard\DataTransferToObject\Sessions\LoginData;
+
+final class LoginAction
+{
+    public function __invoke(LoginData $data): Admin
+    {
+        $admin = Admin::where('phone', $data->phone)->first();
+
+        throw_if(!password_verify($data->password, $admin->password), new LogicException(__('auth.failed'), 403));
+
+        $admin['token'] = $admin->createToken('adminAccessToken')->plainTextToken;
+
+        return $admin;
+    }
+}
