9ecd453a988485b46a4e4a897d2aa797fa4e0b2d
Mohammed Zubair <moh.775123554@gmail.com>
2023-11-28 16:36:37 +0300

    Add  endpoint

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\v1\Dashboard\SessionController;
+use App\Http\Controllers\Api\v1\Dashboard\ShopController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('session.')
@@ -8,4 +9,18 @@ Route::name('session.')
     ->controller(SessionController::class)->group(function () {
         Route::post('login', 'login');
         Route::post('logout', 'logout')->middleware('auth:admin-api');
+    });
+
+Route::middleware('auth:admin-api')
+    ->group(function () {
+        
+        Route::name('shops.')
+            ->prefix('shops')
+            ->controller(ShopController::class)->group(function () {
+                Route::get('/', 'index');
+                Route::post('/', 'store');
+                Route::get('/{id}', 'show');
+                Route::patch('/{id}', 'update');
+                Route::delete('/{id}', 'destroy');
+            });
     });
