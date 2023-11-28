2777d7b3fee0f13dd86db3699381b530da59c35e
Mohammed Zubair <moh.775123554@gmail.com>
2023-11-28 16:34:41 +0300

    Add migrations

---
+++ database/migrations/2023_11_28_073402_create_shops_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('owner_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('password');
+            $table->text('description');
+            $table->string('address'); //TODO it will foreign key
+            $table->boolean('is_active')->default(true);
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shops');
+    }
+};
