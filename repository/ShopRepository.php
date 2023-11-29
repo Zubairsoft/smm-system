92f12a6efce0d2084bb63b585db77d385c6508f2
Mohammed Zubair <moh.775123554@gmail.com>
2023-11-29 12:33:29 +0300

    Update shop repo

--- repository/ShopRepository.php
+++ repository/ShopRepository.php
@@ -4,48 +4,14 @@ namespace Repository;
 
 use App\Models\Shop;
 
-class ShopRepository
+class ShopRepository extends BaseRepository
 {
-    private $model = Shop::class;
+    protected $model;
 
-    public function index($data)
+    protected function setData()
     {
-
+        $this->model = Shop::class;
     }
 
-    public function store($data, string $file): Shop
-    {
-        $shop = app($this->model)->query()->create($data->toArray());
 
-        if (isFile($data->{$file})) {
-            $shop->addMedia($data->{$file})->toMediaCollection('avatar');
-        }
-
-        return $shop;
-    }
-
-    public function show($id): Shop
-    {
-        return app($this->model)->query()->findOrFail($id);
-    }
-
-    public function update($data, string $id, string $file): Shop
-    {
-        $shop = app($this->model)->query()->findOrFail($id);
-
-        $shop->update($data->toArray());
-
-        if (isFile($data->{$file})) {
-            $shop->addMedia($data->{$file})->toMediaCollection('avatar');
-        }
-
-        return $shop->refresh();
-    }
-
-    public function destroy($id): void
-    {
-        $shop = app($this->model)->query()->findOrFail($id);
-
-        $shop->delete();
-    }
 }
