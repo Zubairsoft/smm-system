cc46adb5077b0b907ce268cfe4e8a029a50e148a
Mohammed Zubair <moh.775123554@gmail.com>
2023-11-29 15:58:48 +0300

    Add store bank account data with handle functionality

---
+++ domain/Dashboard/DataTransferToObject/BankAccounts/StoreBankAccountData.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Domain\Dashboard\DataTransferToObject\BankAccounts;
+
+use Domain\Supports\Concerns\Requests\HasFailedValidationDtoRequest;
+use Domain\Supports\Enums\CurrencyEnum;
+use Illuminate\Validation\Rule;
+use Spatie\LaravelData\Data;
+
+class StoreBankAccountData extends Data
+{
+    use HasFailedValidationDtoRequest;
+
+    public function __construct(
+        public string $bank_id,
+        public string $account_number,
+        public string $currency,
+    ) {
+    }
+
+    public static function rules(): array
+    {
+        return [
+            'bank_id' => [
+                'required',
+                'max:100',
+            ],
+            'account_number' => [
+                'required',
+                'numeric'
+            ],
+            'currency' => [
+                'required',
+                Rule::in(CurrencyEnum::getKeys()),
+            ],
+        ];
+    }
+}
