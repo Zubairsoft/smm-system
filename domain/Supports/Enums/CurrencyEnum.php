ed02a8c64d406d70f21e821c6b1e61ac37ca96ed
Mohammed Zubair <moh.775123554@gmail.com>
2023-11-29 15:57:38 +0300

    Add currency enum

---
+++ domain/Supports/Enums/CurrencyEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Domain\Supports\Enums;
+
+use BenSampo\Enum\Enum;
+
+class CurrencyEnum extends Enum
+{
+    const YER = 1;
+    const SAR = 2;
+    const UAE = 3;
+    const USD = 4;
+}
