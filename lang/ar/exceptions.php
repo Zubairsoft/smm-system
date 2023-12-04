b694566455dd81a7415b6e9d8aec5362485d930e
Mohammed Zubair <moh.775123554@gmail.com>
2023-12-04 16:55:01 +0300

    update localization file

--- lang/ar/exceptions.php
+++ lang/ar/exceptions.php
@@ -53,5 +53,6 @@ return [
     'unpublished_event_enrollment' => 'لا يمكنك تقديم طلب إشتراك لفعالية غير منشورة',
     'enrollment_request_already_sent' => 'لقد تم إرسال طلب الإشتراك مسبقاً',
     'enrollment_request_already_accepted' => 'لقد تم قبول طلب الإشتراك مسبقاً',
-    'is_not_active_account'=>'حسابك متوقف يرجى التواصل مع المسسؤل'
+    'is_not_active_account'=>'حسابك متوقف يرجى التواصل مع المسسؤل',
+    //'must_implemented_has_media'
 ];
