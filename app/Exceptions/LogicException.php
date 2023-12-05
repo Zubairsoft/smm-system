f6ba3c59274389ea420c0401c0f72503ad812d8d
Mohammed Zubair <moh.775123554@gmail.com>
2023-12-05 16:43:07 +0300

    Update logic exception

--- app/Exceptions/LogicException.php
+++ app/Exceptions/LogicException.php
@@ -6,9 +6,17 @@ use Exception;
 
 class LogicException extends Exception
 {
+    private $errorKey;
 
-    public function __construct($message = 'error', $statusCode = 400)
+    public function __construct($message = 'error', $statusCode = 400, string $errorKey = 'error')
     {
         parent::__construct($message, $statusCode);
+
+        $this->errorKey = $errorKey;
+    }
+
+    public function getErrorKey()
+    {
+        return $this->errorKey;
     }
 }
