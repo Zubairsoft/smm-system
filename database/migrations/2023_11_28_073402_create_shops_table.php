712edec0b4fc91dcc14489cb74edef15fa5349e3
Mohammed Zubair <moh.775123554@gmail.com>
2023-12-06 13:17:13 +0300

    Update migration

--- database/migrations/2023_11_28_073402_create_shops_table.php
+++ database/migrations/2023_11_28_073402_create_shops_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->string('password');
             $table->text('description');
             $table->string('address'); //TODO it will foreign key
-            $table->boolean('is_active')->default(true);
+            $table->boolean('is_active')->default(false);
 
             $table->softDeletes();
             $table->timestamps();
---
+++ database/migrations/2023_12_05_100656_create_verify_emails_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('verify_emails', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuidMorphs('verifiable');
+            $table->string('email');
+            $table->string('otp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('verify_emails');
+    }
+};
