e80ffd2fb1ada68cb57f360ad3fc1edf89ffc9d0
Mohammed Zubair <moh.775123554@gmail.com>
2023-12-09 22:12:59 +0300

    Update register functionality

--- domain/Shops/Actions/Sessions/RegisterAction.php
+++ domain/Shops/Actions/Sessions/RegisterAction.php
@@ -5,12 +5,15 @@ namespace Domain\Shops\Actions\Sessions;
 use App\Models\Shop;
 use Domain\Shops\DataTransferToObject\Sessions\RegisterData;
 use Helper\Classes\File;
+use Helper\Classes\Verification;
 
 class RegisterAction
 {
     public function __invoke(RegisterData $data): Shop
     {
-        $shop = Shop::query()->create($data->toArray());
+        $shop = Shop::query()->create($data->except('phone')->toArray());
+
+        Verification::sendVerificationPhoneCode(model:$shop,phone:$data->phone);
 
         File::StoreMedia($shop, $data->avatar, 'avatar');
 
