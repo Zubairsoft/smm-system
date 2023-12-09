df579bd6ec5a7dcac3250b517247da0192f5bd8b
Mohammed Zubair <moh.775123554@gmail.com>
2023-12-09 22:13:50 +0300

    Update session controller

--- app/Http/Controllers/Api/v1/Shops/SessionController.php
+++ app/Http/Controllers/Api/v1/Shops/SessionController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers\Api\v1\Shops;
 
 use App\Http\Controllers\Controller;
 use Domain\Dashboard\Actions\Sessions\LogoutAction;
+use Domain\Shops\Actions\Sessions\ActivateEmailAction;
+use Domain\Shops\Actions\Sessions\ActivatePhoneAction;
 use Domain\Shops\Actions\Sessions\LoginAction;
 use Domain\Shops\DataTransferToObject\Sessions\LoginData;
 use Domain\Shops\Actions\Sessions\RegisterAction;
 use Domain\Shops\Actions\Sessions\ResendEmailVerificationCodeAction;
+use Domain\Shops\Actions\Sessions\ResendPhoneVerificationCodeAction;
+use Domain\Shops\DataTransferToObject\Sessions\ActivateEmailData;
+use Domain\Shops\DataTransferToObject\Sessions\ActivatePhoneData;
 use Domain\Shops\DataTransferToObject\Sessions\RegisterData;
 use Domain\Shops\DataTransferToObject\Sessions\ResendEmailVerificationCodeData;
+use Domain\Shops\DataTransferToObject\Sessions\ResendPhoneVerificationCodeData;
 use Illuminate\Http\JsonResponse;
 
 class SessionController extends Controller
@@ -21,7 +27,6 @@ class SessionController extends Controller
         return sendSuccessResponse(message: __('auth.success_register'), data: $shop);
     }
 
-
     public function login(LoginData $request): JsonResponse
     {
         $shop = app(LoginAction::class)($request);
@@ -29,11 +34,32 @@ class SessionController extends Controller
         return sendSuccessResponse(message: __('auth.success_register'), data: $shop);
     }
 
-    public function sendEmailVerificationCode(ResendEmailVerificationCodeData $request): JsonResponse
+    public function resendEmailVerificationCode(ResendEmailVerificationCodeData $request): JsonResponse
     {
         $email = app(ResendEmailVerificationCodeAction::class)($request);
 
         return sendSuccessResponse(__('auth.send_verification_code'), $email);
+    }
+
+    public function activateEmail(ActivateEmailData $request): JsonResponse
+    {
+        $email = app(ActivateEmailAction::class)($request);
+
+        return sendSuccessResponse(__('auth.email_verified'), $email);
+    }
+
+    public function resendPhoneVerificationCode(ResendPhoneVerificationCodeData $request): JsonResponse
+    {
+        $phone = app(ResendPhoneVerificationCodeAction::class)($request);
+
+        return sendSuccessResponse(__('auth.send_phone_verification_code'), $phone);
+    }
+
+    public function activatePhone(ActivatePhoneData $request): JsonResponse
+    {
+        $phone = app(ActivatePhoneAction::class)($request);
+
+        return sendSuccessResponse(__('auth.phone_verified'), $phone);
     }
 
     public function logout(): JsonResponse
