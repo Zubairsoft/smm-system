88a3cd1fc34cccf17994d596d0275610c0f357fb
Mohammed Zubair <moh.775123554@gmail.com>
2023-12-13 12:56:22 +0300

    Add bank account controller with handle functionality

--- app/Http/Controllers/Api/v1/Shops/BankAccountController.php
+++ app/Http/Controllers/Api/v1/Shops/BankAccountController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers\Api\v1\Shops;
 
 use App\Http\Controllers\Controller;
-use App\Models\Shop;
+use App\Http\Resources\Shops\BanKAccounts\BankAccountResource;
+use App\Http\Resources\Shops\BanKAccounts\BankAccountsResource;
 use Domain\Shops\DataTransferToObject\BankAccounts\StoreBankAccountData;
 use Domain\Shops\DataTransferToObject\BankAccounts\UpdateBankAccountData;
 use Illuminate\Http\JsonResponse;
@@ -11,47 +12,42 @@ use Repository\BankAccountRepository;
 
 class BankAccountController extends Controller
 {
-    private Shop  $currentUser;
 
-    private BankAccountRepository $repository;
-
-    public function __construct()
+    public function __construct(private BankAccountRepository $repository)
     {
-        $this->repository = new BankAccountRepository();
-        $this->currentUser = currentUser('shop-api');
     }
 
     public function index(): JsonResponse
     {
-        $bankAccounts = $this->repository->index($this->currentUser->id);
+        $bankAccounts = $this->repository->index($this->currentUser('shop-api')->id);
 
-        return sendSuccessResponse(__('messages.get_data'), $bankAccounts);
+        return sendSuccessResponse(__('messages.get_data'), BankAccountsResource::collection($bankAccounts));
     }
 
     public function store(StoreBankAccountData $request): JsonResponse
     {
-        $bankAccount = $this->repository->store($request, $this->currentUser->id);
+        $bankAccount = $this->repository->store($request, $this->currentUser('shop-api')->id);
 
-        return sendSuccessResponse(__('messages.create_data'), $bankAccount);
+        return sendSuccessResponse(__('messages.create_data'), BankAccountResource::make($bankAccount));
     }
 
     public function show(string $id): JsonResponse
     {
-        $bankAccount = $this->repository->show($this->currentUser->id, $id);
+        $bankAccount = $this->repository->show($this->currentUser('shop-api')->id, $id);
 
-        return sendSuccessResponse(__('messages.get_data'), $bankAccount);
+        return sendSuccessResponse(__('messages.get_data'), BankAccountResource::make($bankAccount));
     }
 
     public function update(UpdateBankAccountData $request, string $id): JsonResponse
     {
-        $bankAccount = $this->repository->update($request, $this->currentUser->id, $id);
+        $bankAccount = $this->repository->update($request, $this->currentUser('shop-api')->id, $id);
 
-        return sendSuccessResponse(__('messages.create_data'), $bankAccount);
+        return sendSuccessResponse(__('messages.update_data'), BankAccountResource::make($bankAccount));
     }
 
     public function destroy(string $id)
     {
-        $this->repository->destroy($this->currentUser->id, $id);
+        $this->repository->destroy($this->currentUser('shop-api')->id, $id);
 
         return sendSuccessResponse(__('messages.delete_data'));
     }
