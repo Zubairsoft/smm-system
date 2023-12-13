9500ee1e6469fd73ce60f6c9e15eb1d658c7df37
Mohammed Zubair <moh.775123554@gmail.com>
2023-12-13 12:53:52 +0300

    Update controller add current user

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Admin;
+use App\Models\Shop;
+use App\Models\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -9,4 +12,9 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    protected function currentUser(string $guard): Admin|Shop|User
+    {
+        return currentUser($guard);
+    }
 }
