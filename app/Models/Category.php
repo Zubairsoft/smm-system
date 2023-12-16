abd213591e415a5513e6888e6da83348fe651347
Mohammed Zubair <moh.775123554@gmail.com>
2023-12-16 20:49:55 +0300

    Add category model

---
+++ app/Models/Category.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Category extends BaseModel implements HasMedia
+{
+    use InteractsWithMedia;
+    
+    protected $fillable = [
+        'name_ar',
+        'name_en',
+        'is_active'
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean'
+    ];
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('image')
+            ->singleFile();
+
+        $this->addMediaCollection('cover_image')
+            ->singleFile();
+    }
+}
