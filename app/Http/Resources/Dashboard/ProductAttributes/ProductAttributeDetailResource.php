bfc5445a5ee694c5aaca7d009c34c18c510c0777
Mohammed Zubair <moh.775123554@gmail.com>
2023-12-19 16:55:34 +0300

    Add resources

---
+++ app/Http/Resources/Dashboard/ProductAttributes/ProductAttributeDetailResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Dashboard\ProductAttributes;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductAttributeDetailResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name_ar' => $this->name_ar,
+            'name_en' => $this->name_en,
+            'is_active' => $this->is_active,
+        ];
+    }
+}
