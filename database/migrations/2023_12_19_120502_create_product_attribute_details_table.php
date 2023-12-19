8411fab5f3a6a9baf69f4796abc84a55f01ef3ed
Mohammed Zubair <moh.775123554@gmail.com>
2023-12-19 16:53:50 +0300

    Add migrations

---
+++ database/migrations/2023_12_19_120502_create_product_attribute_details_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_attribute_details', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('product_attribute_id');
+            $table->string('name_ar');
+            $table->string('name_en');
+            $table->boolean('is_active')->default(false);
+
+            $table->foreign('product_attribute_id')->references('id')->on('product_attributes')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_attribute_details');
+    }
+};
