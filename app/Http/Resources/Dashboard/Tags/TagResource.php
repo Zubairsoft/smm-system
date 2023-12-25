6702c903c0456d1eb15c02247e2c255575fefc72
Mohammed Zubair <moh.775123554@gmail.com>
2023-12-25 19:44:22 +0300

    Add tag resources

---
+++ app/Http/Resources/Dashboard/Tags/TagResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Dashboard\Tags;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TagResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name_ar' => $this->name_ar,
+            'name_en' => $this->name_en,
+            'is_active' => $this->is_active,
+        ];
+    }
+}
