bcf2ae131f69afa4808dc7afc89b6f696bdea56d
Mohammed Zubair <moh.775123554@gmail.com>
2023-12-26 15:56:24 +0300

    Add endpoint

--- routes/support.php
+++ routes/support.php
@@ -1,10 +1,18 @@
 <?php
 
 use App\Http\Controllers\Api\v1\Supports\lists\ListBankController;
+use App\Http\Controllers\Api\v1\Supports\lists\ListBrandController;
+use App\Http\Controllers\Api\v1\Supports\lists\ListCategoryController;
+use App\Http\Controllers\Api\v1\Supports\lists\ListProductAttributeController;
+use App\Http\Controllers\Api\v1\Supports\lists\ListProductAttributeDetailController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('lists')
     ->group(function () {
         Route::get('banks', ListBankController::class);
+        Route::get('brands', ListBrandController::class);
+        Route::get('categories', ListCategoryController::class);
+        Route::get('product_attributes', ListProductAttributeController::class);
+        Route::get('product_attributes/{id}/product_attribute_details', ListProductAttributeDetailController::class);
     });
