54cb15e88dbbede21b62a64ce864f05310893efe
Mohammed Zubair <moh.775123554@gmail.com>
2024-01-02 14:10:45 +0300

    Add data

---
+++ domain/Shops/DataTransferToObject/Cobones/UpdateCoboneData.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Domain\Shops\DataTransferToObject\Cobones;
+
+use Domain\Shops\Enums\CoboneTypeEnum;
+use Domain\Supports\Concerns\Requests\HasFailedValidationDtoRequest;
+use Illuminate\Validation\Rule;
+use Spatie\LaravelData\Attributes\MapInputName;
+use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Optional;
+
+class UpdateCoboneData extends Data
+{
+    use HasFailedValidationDtoRequest;
+
+    public function __construct(
+        #[MapInputName('cobone_type')]
+        public Optional|int $type,
+        public Optional|string $start_at,
+        public Optional|string $end_at,
+        public Optional|float $total_order
+
+    ) {
+    }
+
+    public static function rules(): array
+    {
+        return [
+            'cobone_type' => [
+                Rule::in(CoboneTypeEnum::getValues())
+            ],
+            'start_at' => [
+                'after_or_equal:today',
+                'date_format:Y-m-d'
+            ],
+            'end_at' => [
+                'after:start_at',
+                'date_format:Y-m-d'
+            ],
+            'total_order' => [
+                'numeric',
+                'min:1',
+                Rule::requiredIf(request()->cobone_type ? CoboneTypeEnum::TOTAL_ORDER()->is((int)request()->cobone_type) : false),
+                Rule::prohibitedIf(request()->cobone_type ? CoboneTypeEnum::SPECIFIC_PRODUCT()->is((int)request()->cobone_type) : false)
+            ],
+        ];
+    }
+}
