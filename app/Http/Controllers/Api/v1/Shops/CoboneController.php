c558125dd1259cb59d364a3b5b9371a32130f1d9
Mohammed Zubair <moh.775123554@gmail.com>
2024-01-02 14:11:36 +0300

    Update controller add resource

--- app/Http/Controllers/Api/v1/Shops/CoboneController.php
+++ app/Http/Controllers/Api/v1/Shops/CoboneController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api\v1\Shops;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Shops\Cobones\CoboneResource;
 use Domain\Shops\DataTransferToObject\Cobones\StoreCoboneData;
+use Domain\Shops\DataTransferToObject\Cobones\UpdateCoboneData;
 use Repository\CoboneRepository;
 
 class CoboneController extends Controller
@@ -16,28 +18,28 @@ class CoboneController extends Controller
     {
         $cobones = $this->repository->index(currentUser(config('auth.shop-api-guard'))->id);
 
-        return sendSuccessResponse(__('messages.get_data'), $cobones);
+        return sendSuccessResponse(__('messages.get_data'), CoboneResource::collection($cobones));
     }
 
     public function store(StoreCoboneData $request)
     {
         $cobone = $this->repository->store($request, currentUser(config('auth.shop-api-guard'))->id);
 
-        return sendSuccessResponse(__('messages.create_data'), $cobone);
+        return sendSuccessResponse(__('messages.create_data'), CoboneResource::make($cobone));
     }
 
     public function show(string $id)
     {
         $cobone = $this->repository->show(currentUser(config('auth.shop-api-guard'))->id, $id);
 
-        return sendSuccessResponse(__('messages.get_data'), $cobone);
+        return sendSuccessResponse(__('messages.get_data'), CoboneResource::make($cobone));
     }
 
-    public function update(StoreCoboneData $request, string $id)
+    public function update(UpdateCoboneData $request, string $id)
     {
         $cobone = $this->repository->update($request, currentUser(config('auth.shop-api-guard'))->id, $id);
 
-        return sendSuccessResponse(__('messages.update_data'), $cobone);
+        return sendSuccessResponse(__('messages.update_data'), CoboneResource::make($cobone));
     }
 
     public function destroy(string $id)
