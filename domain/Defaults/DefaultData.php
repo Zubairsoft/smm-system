5b7b3b7975032eb15c9f3af1622bcf5fbed8102f
Mohammed Zubair <moh.775123554@gmail.com>
2024-01-08 11:21:16 +0300

    use data in default data

--- domain/Defaults/DefaultData.php
+++ domain/Defaults/DefaultData.php
@@ -4,10 +4,12 @@ namespace Domain\Defaults;
 
 use Domain\Defaults\Data\BankData;
 use Domain\Defaults\Data\BrandData;
+use Domain\Defaults\Data\ProductAttributeData;
+use Domain\Defaults\Data\ProductAttributeDetailData;
 
 class DefaultData
 {
-    use BankData, BrandData;
+    use BankData, BrandData, ProductAttributeData, ProductAttributeDetailData;
 
     public static function categories()
     {
