8d58bd19faf3ebb27978aa320a51159a5208099b
Mohammed Zubair <moh.775123554@gmail.com>
2024-01-09 22:05:48 +0300

    Add profile controller with handle functionality

--- app/Http/Controllers/Api/v1/Shops/ProfileController.php
+++ app/Http/Controllers/Api/v1/Shops/ProfileController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api\v1\Shops;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Shops\Profiles\ProfileResource;
+use App\Models\Shop;
 use Domain\Shops\DataTransferToObject\Profiles\UpdateProfileData;
 use Domain\Supports\Actions\UpdateProfileAction;
 use Illuminate\Http\JsonResponse;
@@ -11,13 +13,18 @@ class ProfileController extends Controller
 {
     public function show(): JsonResponse
     {
-        return sendSuccessResponse(__('messages.get_data'), currentUser(config('auth.shop-api-guard')));
+        return sendSuccessResponse(__('messages.get_data'), ProfileResource::make($this->getCurrentShop()));
     }
 
     public function update(UpdateProfileData $request): JsonResponse
     {
-        $shop = app(UpdateProfileAction::class)($request);
+        $shop = app(UpdateProfileAction::class)($this->getCurrentShop(), $request);
 
-        return sendSuccessResponse(__('messages.update_data'), $shop);
+        return sendSuccessResponse(__('messages.update_data'), ProfileResource::make($shop));
+    }
+
+    private function getCurrentShop(): Shop
+    {
+        return  currentUser(config('auth.shop-api-guard'));
     }
 }
