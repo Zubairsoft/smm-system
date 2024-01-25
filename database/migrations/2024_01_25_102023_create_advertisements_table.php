3ed644f8692f3adad9193251bb88775599241466
Mohammed Zubair <moh.775123554@gmail.com>
2024-01-25 16:50:19 +0300

    Add migration

---
+++ database/migrations/2024_01_25_102023_create_advertisements_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('advertisements', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->text('text');
+            $table->dateTime('expire_at');
+            $table->boolean('is_active')->default(false);
+            $table->timestamps();
+            $table->index('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('advertisements');
+    }
+};
