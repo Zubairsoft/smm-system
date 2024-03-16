8ca971f8c6c0558550b092bf88c3feb95809ccff
Mohammed Zubair <moh.775123554@gmail.com>
2024-03-16 13:37:53 +0300

    Update transaction action

--- domain/Dashboard/Actions/Transactions/IndexTransactionAction.php
+++ domain/Dashboard/Actions/Transactions/IndexTransactionAction.php
@@ -3,28 +3,14 @@
 namespace Domain\Dashboard\Actions\Transactions;
 
 use App\Models\Transaction;
-use Domain\Dashboard\DataTransferToObject\Wallets\IndexTransactionData;
+use Domain\Supports\DataTransferToObject\Transactions\IndexTransactionData;
 use Illuminate\Database\Eloquent\Builder;
+use Repository\TransactionRepository;
 
 class IndexTransactionAction
 {
     public function __invoke(IndexTransactionData $data, string $id)
     {
-        $transaction = Transaction::query()->where('from_id', $id)
-            ->orWhere('to_id', $id)->sort(['created_at'], $data->sorts);
-
-        $this->fillerDate($transaction, $data->date);
-
-
-        return $transaction->sort(['created_at', $data->sorts])->paginate($data->per_page);
-    }
-
-    private function fillerDate(Builder $query, array $data): Builder
-    {
-        if (count($data) > 0) {
-            return $query->whereBetween('created_at', $data);
-        }
-
-        return $query;
+        return (new TransactionRepository())->index($data, $id);
     }
 }
