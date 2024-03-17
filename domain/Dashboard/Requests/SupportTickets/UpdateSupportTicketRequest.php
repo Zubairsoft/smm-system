a3e7df4a0e7a84977ad673a83b2b3c156a0a3433
Mohammed Zubair <moh.775123554@gmail.com>
2024-03-18 00:49:45 +0300

    Add request with handle functionality

---
+++ domain/Dashboard/Requests/SupportTickets/UpdateSupportTicketRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Domain\Dashboard\Requests\SupportTickets;
+
+use Domain\Shops\Enums\SupportTicketEnum;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
+
+class UpdateSupportTicketRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'reply' => [
+                'string',
+                'max:255'
+            ],
+            'status' => [
+                'required',
+                Rule::in(SupportTicketEnum::getKeys())
+            ]
+        ];
+    }
+
+    protected function failedValidation(Validator $validator): void
+    {
+        if ($validator->fails()) {
+            throw new HttpResponseException(
+                sendFailedResponse(
+                    __('messages.validation_error'),
+                    $validator->errors()->first(),
+                    422
+                )
+            );
+        }
+    }
+}
