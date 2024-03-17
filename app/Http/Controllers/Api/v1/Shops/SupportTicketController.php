b9e677f5b98a624423e03cd040917819e0672098
Mohammed Zubair <moh.775123554@gmail.com>
2024-03-18 00:46:56 +0300

    Add support ticket data with handle functionality

---
+++ app/Http/Controllers/Api/v1/Shops/SupportTicketController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Shops;
+
+use App\Http\Controllers\Controller;
+use Domain\Shops\Actions\SupportTickets\StoreSupportTicketAction;
+use Domain\Shops\DataTransferToObject\SupportTickets\StoreSupportTicketData;
+use Illuminate\Http\JsonResponse;
+use Repository\SupportTicketRepository;
+
+class SupportTicketController extends Controller
+{
+    public function __construct(
+        private SupportTicketRepository $repository
+    ) {
+    }
+    public function store(StoreSupportTicketData $request): JsonResponse
+    {
+        $shop = currentUser('shop-api');
+
+        $supportTicket = (new StoreSupportTicketAction)($shop, $request);
+
+        $this->repository->addMedia($request, $supportTicket, 'attachment', 'attachment');
+
+        return sendSuccessResponse(__('messages.create_data'), $supportTicket);
+    }
+}
