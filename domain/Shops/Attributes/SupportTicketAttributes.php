c99cc96055352e779767bc12d51cd468e60d0716
Mohammed Zubair <moh.775123554@gmail.com>
2024-03-18 02:10:44 +0300

    Update attribute

--- domain/Shops/Attributes/SupportTicketAttributes.php
+++ domain/Shops/Attributes/SupportTicketAttributes.php
@@ -6,11 +6,6 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 
 trait SupportTicketAttributes
 {
-    protected function status(): Attribute
-    {
-        return new Attribute(get: fn ($value) => $value->description);
-    }
-
     protected function attachment(): Attribute
     {
         return new Attribute(get: fn () => $this->getFirstMediaUrl('attachment'));
